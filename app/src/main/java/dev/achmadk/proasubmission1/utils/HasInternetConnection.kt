62f5df740da1ba7bafb7bbec7e46bdad0f4b0bd6
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2022-11-14 10:41:25 +0700

    fix(unit testing): :ambulance: fix return null when call `hasInternetConnection`
    temporarily return true when call hasInternetConnection inside unit testing

--- app/src/main/java/dev/achmadk/proasubmission1/utils/HasInternetConnection.kt
+++ app/src/main/java/dev/achmadk/proasubmission1/utils/HasInternetConnection.kt
@@ -1,13 +1,16 @@
 package dev.achmadk.proasubmission1.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
 
+@SuppressLint("MissingPermission")
 @Suppress("DEPRECATION")
 fun hasInternetConnection (context: Context): Boolean {
-    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        ?: return true
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
         val activeNetwork = connectivityManager.activeNetwork ?: return false
